ecbb6aca63d67344158e823a630042186a3728be
AndreyHerzhov <andreyherzhov@gmail.com>
2022-07-09 20:09:58 +0300

    markup_render

---
+++ src/js/refs.js
@@ -0,0 +1,12 @@
+export const refs = {
+    btnVoting: document.querySelector('[data-search="voting"]'),
+    btnBreeds: document.querySelector('[data-search="breeds"]'),
+    btnGallery: document.querySelector('[data-search="gallery"]'),
+    main: document.querySelector('.main-container'),
+    backgroundMain: document.querySelector('.main-container_background'),
+    input: document.querySelector('.search-form__field'),
+    logo: document.querySelector('.nav-logo'),
+    btnBack: document.querySelector('[data-search="voting"]'),
+    searchBtn: document.querySelector('.voting-button_search'),
+    votingInput: document.querySelector('.main-container_voting-form')
+} 
